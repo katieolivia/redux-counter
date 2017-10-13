9344ddecd96e7913af9920ceb887c7ca24acbd37
Kathryn Nutzman <katieonutzman@Kathryns-MacBook-Pro.local>
2017-10-12 18:53:51 -0600

    finished

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,80 @@
 // change this to true to see test results on the black diamond section.
-export const BLACK_DIAMOND = false;
+export const BLACK_DIAMOND = true;
+
+const initialState = {
+    currentValue: 0,
+    previousValue: [],
+    futureValue: []
+}
+
+const INCREMENT = 'INCREMENT';
+
+const DECREMENT = 'DECREMENT';
+
+const UNDO = 'UNDO';
+
+const REDO = 'REDO';
+
+
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            return { 
+                currentValue: state.currentValue + action.amount,
+                previousValue: [state.currentValue, ...state.previousValue],
+                futureValue: []
+            };
+        case DECREMENT:
+            return { 
+                currentValue: state.currentValue - action.amount,
+                previousValue: [state.currentValue, ...state.previousValue],
+                futureValue: [] 
+            };
+        case UNDO:
+            return {
+                currentValue: state.previousValue[0],
+                previousValue: state.previousValue.slice(1),
+                futureValue: [state.currentValue, ...state.futureValue]
+            };
+        case REDO:
+            return {
+                currentValue: state.futureValue[0],
+                previousValue: [state.currentValue, ...state.previousValue],
+                futureValue: state.futureValue.slice(1)
+            };
+
+        default:
+            return state;
+    }
+}
+
+export function increment(amount) {
+    return {
+        type: INCREMENT,
+        amount: amount
+    }
+}
+
+export function decrement(amount) {
+    return {
+        type: DECREMENT,
+        amount: amount
+    }
+}
+
+export function undo() {
+    return {
+        type: UNDO
+    }
+    
+}
+
+
+export function redo() {
+    return {
+        type: REDO
+    }
+
+}
+
+
